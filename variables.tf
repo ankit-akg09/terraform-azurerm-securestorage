5e23cce76bda1a98b86d2e1a5e8146547298aa45
Ankit <ankit_akg09@yahoo.com>
2025-12-31 19:32:48 +0530

    Commit

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type        = string
+  description = "resource group name"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "storage account name"
+}
+
+variable "location" {
+  type        = string
+  description = "location name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name"
+}
